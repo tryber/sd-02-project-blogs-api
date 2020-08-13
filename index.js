80b9eb45806bb6eef727f30a477a527bbfce1689
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-08-13 01:45:03 -0300

    index e variáveis

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const controllers = require('./controllers');
+const middlewares = require('./middlewares');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/login', controllers.loginUser);
+
+app.get('/user', middlewares.validateJWT, controllers.getAllUsers);
+app.get('/user/:id', middlewares.validateJWT, controllers.getUserById);
+
+app.use(middlewares.promiseErrors);
+
+app.all('*', middlewares.endpointNotFound);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Ouvindo porta ${PORT}!`));
