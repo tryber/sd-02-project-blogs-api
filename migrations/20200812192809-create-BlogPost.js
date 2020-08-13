3a4f05b9d741d761d2b5d05a3d76373ad55d9ac5
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-08-13 01:42:43 -0300

    Migrations criadas

---
+++ migrations/20200812192809-create-BlogPost.js
@@ -0,0 +1,20 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPost', {
+      id: { autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
+      title: { allowNull: false, type: Sequelize.STRING },
+      content: { allowNull: false, type: Sequelize.STRING },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: 'Users', key: 'id' },
+      },
+      published: { type: Sequelize.DATE },
+      updated: { type: Sequelize.DATE },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('BlogPost');
+  },
+};
