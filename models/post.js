07640f28a02852673787fbb1ce268e3b791c6eb2
phtf92 <phtf92@gmail.com>
2020-08-13 22:17:46 -0300

    Models

---
+++ models/post.js
@@ -0,0 +1,25 @@
+const createPostModel = (sequelize, DataTypes) => {
+  const Post = sequelize.define('Post', {
+    content: DataTypes.STRING,
+    title: DataTypes.STRING,
+    user_id: {
+      type: DataTypes.STRING,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+    },
+  }, {
+    timestamps: false,
+    createdAt: 'published',
+    updatedAt: 'updated',
+  });
+
+  Post.associate = (models) => {
+    Post.belongsTo(models.User, { as: 'user' });
+  };
+
+  return Post;
+};
+
+module.exports = createPostModel;
---
+++ models/user.js
@@ -0,0 +1,18 @@
+const createUserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  }, {
+    timestamps: false,
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.Post, { as: 'posts' });
+  };
+
+  return User;
+};
+
+module.exports = userModel;
