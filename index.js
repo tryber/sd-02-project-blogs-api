d348bdc48565c15a2f4e093a4c80c4df00a52009
phtf92 <phtf92@gmail.com>
2020-08-15 01:57:01 -0300

    Creating login route and errorController

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const usersRoute = require('./routes/usersRoutes');
+const { login } = require('./controllers/authenticatorController');
+const { errorController } = require('./controllers/errorController');
 
 const app = express();
 
@@ -9,6 +11,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/user', usersRoute);
+app.post('/login', login);
+
+app.use(errorController);
 
 app.listen(3000, () => {
   console.log('App ouvindo a porta 3000!');
