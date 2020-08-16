9eae9bf38c2fe23cfe66d14d043451bd81520393
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-08-16 02:08:11 -0300

    Refatorando para o CodeClimate

--- controllers/userController.test.js
+++ controllers/userController.test.js
@@ -1,33 +1,16 @@
 const userController = require('./userController');
-const userService = require('../services/userService');
 const { User } = require('../models');
 
 describe('userController tests', () => {
   describe('Login User', () => {
-    test('If pass invalid requisition, return a Joi message and status 422', async () => {
+    test('If pass invalid requisition, return a Joi message and status 400', async () => {
       const mockBodyData = { email: 'johnatas@email.cxom', password: '131072' };
-
       const mockReq = { body: mockBodyData };
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
       const mockNext = jest.fn();
-      const mockJoi = {
-        code: 'invalid_data', error: true, message: '"email" must be a valid email',
-      };
-
-      const userData = {
-        id: 2,
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        password: '524288',
-        image: null,
-      };
-
-      const mockSequelize = {
-        dataValues: userData, ...userData,
-      };
-
-      const loginUserSpy = jest.spyOn(User, 'findOne').mockReturnValueOnce(mockSequelize);
+      const mockJoi = { code: 'bad_request', error: true, message: '"email" must be a valid email' };
+      const loginUserSpy = jest.spyOn(User, 'findOne').mockReturnValueOnce();
 
       await userController.loginUser(mockReq, mockRes, mockNext);
 
@@ -40,28 +23,16 @@ describe('userController tests', () => {
     });
 
     test('If pass invalid credentials, return a service message and status 400', async () => {
-      const mockBodyData = { email: 'johnatas@emai.com', password: '131072' };
-
+      const mockBodyData = { email: 'johnatas@email.com', password: '131072' };
       const mockReq = { body: mockBodyData };
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
       const mockNext = jest.fn();
-      const mockServiceAnswer = {
-        code: 'bad_request', error: true, message: 'Campos inválidos',
-      };
-
+      const mockServiceAnswer = { code: 'bad_request', error: true, message: 'Campos inválidos' };
       const userData = {
-        id: 2,
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        password: '524288',
-        image: null,
+        id: 2, displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.com', password: '524288', image: null,
       };
-
-      const mockSequelize = {
-        dataValues: userData, ...userData,
-      };
-
+      const mockSequelize = { dataValues: userData, ...userData };
       const loginUserSpy = jest.spyOn(User, 'findOne').mockReturnValueOnce(mockSequelize);
 
       await userController.loginUser(mockReq, mockRes, mockNext);
@@ -76,24 +47,14 @@ describe('userController tests', () => {
 
     test('If pass valid credentials, return a token and status 200', async () => {
       const mockBodyData = { email: 'johnatas.henrique@gmail.com', password: '524288' };
-
       const mockReq = { body: mockBodyData };
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
       const mockNext = jest.fn();
-
       const userData = {
-        id: 2,
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        password: '524288',
-        image: null,
+        id: 2, displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.com', password: '524288', image: null,
       };
-
-      const mockSequelize = {
-        dataValues: userData, ...userData,
-      };
-
+      const mockSequelize = { dataValues: userData, ...userData };
       const loginUserSpy = jest.spyOn(User, 'findOne').mockReturnValueOnce(mockSequelize);
 
       await userController.loginUser(mockReq, mockRes, mockNext);
@@ -106,34 +67,23 @@ describe('userController tests', () => {
       loginUserSpy.mockRestore();
     });
   });
+
   describe('Create User', () => {
-    test('If pass invalid requisition, return a Joi message and status 422', async () => {
-      // Arrange
+    test('If pass invalid requisition, return a Joi message and status 400', async () => {
       const mockBodyData = {
         displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.cxom', password: '524288',
       };
-
       const mockReq = { body: mockBodyData };
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
       const mockNext = jest.fn();
-
       const mockJoiAnswer = {
-        code: 'invalid_data', error: true, message: '"email" must be a valid email',
+        code: 'bad_request', error: true, message: '"email" must be a valid email',
       };
-
       const userData = {
-        id: 2,
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        password: '524288',
-        image: null,
+        id: 2, displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.com', password: '524288', image: null,
       };
-
-      const mockSequelize = {
-        dataValues: userData, ...userData,
-      };
-
+      const mockSequelize = { dataValues: userData, ...userData };
       const createUserSpy = jest.spyOn(User, 'create').mockReturnValueOnce(mockSequelize);
       const loginUserSpy = jest.spyOn(User, 'findOne').mockReturnValueOnce(mockSequelize);
 
@@ -150,34 +100,17 @@ describe('userController tests', () => {
 
     test('If pass an user who is in the database, return message and status 409', async () => {
       const mockBodyData = {
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        password: '524288',
+        displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.com', password: '524288',
       };
-
       const mockReq = { body: mockBodyData };
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
       const mockNext = jest.fn();
-
-      const mockServiceAnswer = {
-        error: true,
-        message: 'Usuário já existe',
-        code: 'already_exists',
-      };
-
+      const mockServiceAnswer = { error: true, message: 'Usuário já existe', code: 'already_exists' };
       const userData = {
-        id: 2,
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        password: '524288',
-        image: null,
+        id: 2, displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.com', password: '524288', image: null,
       };
-
-      const mockSequelize = {
-        dataValues: userData, ...userData,
-      };
-
+      const mockSequelize = { dataValues: userData, ...userData };
       const createUserSpy = jest.spyOn(User, 'create').mockReturnValueOnce(mockSequelize);
       const loginUserSpy = jest.spyOn(User, 'findOne').mockReturnValueOnce(mockSequelize);
 
@@ -192,30 +125,18 @@ describe('userController tests', () => {
       loginUserSpy.mockRestore();
     });
 
-    test('If pass valid credentials, return a token and status 200', async () => {
+    test('If pass valid credentials, return a response and status 200', async () => {
       const mockBodyData = {
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        password: '524288',
+        displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.com', password: '524288',
       };
-
       const mockReq = { body: mockBodyData };
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
       const mockNext = jest.fn();
-
       const userData = {
-        id: 2,
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        password: '524288',
-        image: null,
+        id: 2, displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.com', password: '524288', image: null,
       };
-
-      const mockSequelize = {
-        dataValues: userData, ...userData,
-      };
-
+      const mockSequelize = { dataValues: userData, ...userData };
       let findUserSpy = jest.spyOn(User, 'findOne').mockReturnValueOnce(null);
       const createUserSpy = jest.spyOn(User, 'create').mockReturnValueOnce();
       findUserSpy = jest.spyOn(User, 'findOne').mockReturnValueOnce(mockSequelize);
@@ -231,6 +152,7 @@ describe('userController tests', () => {
       createUserSpy.mockRestore();
     });
   });
+
   describe('Get All Users', () => {
     test('Return all users from database and status 200', async () => {
       const mockResponse = [
@@ -248,10 +170,8 @@ describe('userController tests', () => {
           image: null,
         },
       ];
-
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
-
       const getAllUsersSpy = jest.spyOn(User, 'findAll').mockReturnValueOnce(mockResponse);
 
       await userController.getAllUsers(null, mockRes);
@@ -263,16 +183,14 @@ describe('userController tests', () => {
       getAllUsersSpy.mockRestore();
     });
   });
+
   describe('Get User By Id', () => {
     test('If service does not find any info, return a message', async () => {
       const mockReq = { params: { id: 100 } };
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
       const mockNext = jest.fn();
-      const serviceAnswer = {
-        code: 'not_found', error: true, message: 'Usuário não existe',
-      };
-
+      const serviceAnswer = { code: 'not_found', error: true, message: 'Usuário não existe' };
       const getUserByIdSpy = jest.spyOn(User, 'findByPk').mockReturnValueOnce(null);
 
       await userController.getUserById(mockReq, mockRes, mockNext);
@@ -290,16 +208,10 @@ describe('userController tests', () => {
       const mockJson = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ json: mockJson }) };
       const mockNext = jest.fn();
-
       const userData = {
-        id: 2,
-        displayName: 'Johnatas Henrique',
-        email: 'johnatas.henrique@gmail.com',
-        image: null,
+        id: 2, displayName: 'Johnatas Henrique', email: 'johnatas.henrique@gmail.com', image: null,
       };
-
       const mockSequelize = { dataValues: userData };
-
       const getUserByIdSpy = jest.spyOn(User, 'findByPk').mockReturnValueOnce(mockSequelize);
 
       await userController.getUserById(mockReq, mockRes, mockNext);
@@ -312,13 +224,13 @@ describe('userController tests', () => {
       getUserByIdSpy.mockRestore();
     });
   });
+
   describe('Delete User By Id', () => {
     test('When deleting, return an end and status 204', async () => {
       const mockReq = { user: { id: 1 } };
       const mockEnd = jest.fn();
       const mockRes = { status: jest.fn().mockReturnValueOnce({ end: mockEnd }) };
       const mockNext = jest.fn();
-
       const deleteUserByIdSpy = jest.spyOn(User, 'destroy').mockReturnValueOnce();
 
       await userController.deleteUserById(mockReq, mockRes, mockNext);
