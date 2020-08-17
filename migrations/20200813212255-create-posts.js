1f59a23c0f363549f1d0d9915dd502df7a7e668b
phtf92 <phtf92@gmail.com>
2020-08-17 18:41:47 -0300

    CC

--- migrations/20200813212255-create-posts.js
+++ migrations/20200813212255-create-posts.js
@@ -1,45 +1,27 @@
+/* eslint-disable import/no-dynamic-require */
+/* eslint-disable global-require */
+
 'use strict';
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const PostsTable = queryInterface.createTable("Posts", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      content: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      title: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
+      id: { allowNull: false, autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
+      content: { allowNull: false, type: Sequelize.STRING, },
+      title: { allowNull: false, type: Sequelize.STRING, },
       user_id: {
         type: Sequelize.INTEGER,
         references: {
           model: 'Users',
-          key: 'id'
+          key: 'id',
         },
         onUpdate: 'cascade',
-        onDelete: 'cascade'
+        onDelete: 'cascade',
       },
-      published: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-      updated: {
-        type: Sequelize.DATE,
-        allowNull: false
-      }
+      published: { allowNull: true, type: Sequelize.DATE },
+      updated: { type: Sequelize.DATE, allowNull: false },
     });
-
     return PostsTable;
-
   },
-
   down: async (queryInterface) => queryInterface.dropTable("Users"),
-
 };
