6b000785faace5acdea623a59288d69f92190266
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-08-26 10:13:11 -0300

    seeders

--- resource/database/seeders/20200826125355-users.js
+++ resource/database/seeders/20200826125355-users.js
@@ -1,9 +1,18 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Users', []);
+  up: async (queryInterface, _Sequelize) => {
+    await queryInterface.bulkInsert('Users', [
+      {
+        id: 404,
+        displayName: 'Brett Wiltshire',
+        email: 'brett@email.com',
+        password: '123456',
+        image:
+          'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
+      },
+    ]);
   },
 
   down: async (queryInterface, _Sequelize) => {
-    await queryInterface.bulkDelete('Users', null, {});
+    await queryInterface.bulkDelete('Users', { where: { id: 404 } });
   },
 };
