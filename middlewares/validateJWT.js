04a7917a16f758c65e3caa889d963f66ce552d80
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-09-03 01:02:57 -0300

    CC 001

--- middlewares/validateJWT.js
+++ middlewares/validateJWT.js
@@ -6,21 +6,21 @@ const { jwtSecret } = process.env;
 
 const validateJWT = rescue(async (req, _res, next) => {
   const token = req.headers.authorization;
-  if (!token) {
-    return next({ error: true, message: 'Token não encontrado', code: 'unauthorized' });
-  }
+
+  if (!token) return next({ error: true, message: 'Token não encontrado', code: 'unauthorized' });
+
   try {
     const decoded = jwt.verify(token, jwtSecret);
     const userExists = await User.findByPk(decoded.id);
-    if (!userExists) {
-      return next({ error: true, message: 'Usuário não encontrado', code: 'unauthorized' });
-    }
+
+    if (!userExists) return next({ error: true, message: 'Usuário não encontrado', code: 'unauthorized' });
+
     const { dataValues: { password: _, ...userWithoutPassword } } = userExists;
 
     req.user = userWithoutPassword;
-    next();
+    return next();
   } catch (fail) {
-    next({ error: true, message: 'Token expirado ou inválido', code: 'unauthorized' });
+    return next({ error: true, message: 'Token expirado ou inválido', code: 'unauthorized' });
   }
 });
 
