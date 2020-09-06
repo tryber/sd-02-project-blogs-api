50a349a92ca52236f889bfad7cc3261245f97c6a
miguel-miranda <ayandram7727@gmail.com>
2020-09-06 02:21:06 -0300

    CC

--- migrations/20200906033725-create-blog-posts.js
+++ migrations/20200906033725-create-blog-posts.js
@@ -1,41 +1,22 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    const BlogPostsTable = queryInterface.createTable('BlogPosts', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      title: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      content: {
-        allowNull: false,
-        type: Sequelize.STRING,
-      },
-      published: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updated: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
+  up: async (queryInterface, Sequelize) => (
+    queryInterface.createTable('BlogPosts', {
+      id: { allowNull: false, autoIncrement: true, primaryKey: true, type: Sequelize.INTEGER },
+      title: { allowNull: false, type: Sequelize.STRING },
+      content: { allowNull: false, type: Sequelize.STRING },
+      published: { allowNull: false, type: Sequelize.DATE },
+      updated: { allowNull: false, type: Sequelize.DATE },
       user_id: {
         type: Sequelize.INTEGER,
         references: {
           model: 'Users',
           key: 'id',
         },
-        onUpdate: 'cascade',
-        onDelete: 'cascade',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
       },
-    });
-
-    return BlogPostsTable;
-  },
+    })
+  ),
 
   down: async (queryInterface) => queryInterface.dropTable('BlogPosts'),
 };
