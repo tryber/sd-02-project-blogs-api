3a6abb8c561a33985e538ba961b6b920d4893880
miguel-miranda <ayandram7727@gmail.com>
2020-09-07 01:17:07 -0300

    rota POST /user funcionando

---
+++ controllers/userController.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models');
+
+const router = express.Router();
+
+router.post('/', async (req, res, _next) => {
+  const { displayName, email, password, image } = req.body;
+
+  if (!displayName || !email || !password || !image) {
+    return res.status(422).json({ message: 'Dados inválidos' });
+  }
+
+  if (typeof displayName !== 'string' || displayName.length < 8) {
+    return res.status(422).json({ message: 'displayName deve ter pelo menos 8 caracteres' });
+  }
+
+  if (!/[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/.test(email)) {
+    return res.status(422).json({ message: 'Insira um email válido' });
+  }
+
+  if (typeof password !== 'string' || password.length !== 6) {
+    return res.status(422).json({ message: 'A senha deve conter 6 caracteres' });
+  }
+
+  try {
+    const [, created] = await User.findOrCreate({
+      where: { email },
+      defaults: { displayName, password, image },
+    });
+
+    if (!created) return res.status(409).json({ message: 'Usuário já existe' });
+
+    const { dataValues: { password: _, ...user } } = await User.findOne({
+      where: { email },
+    });
+
+    const token = jwt.sign(user, process.env.SECRET, {
+      expiresIn: '50m',
+      algorithm: 'HS256',
+    });
+
+    return res.status(201).json({ token });
+  } catch (error) {
+    return res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
+const controllers = require('./controllers');
 
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/user', controllers.user);
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
