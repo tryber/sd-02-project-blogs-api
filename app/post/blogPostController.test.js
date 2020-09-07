9f5cce9b6d0a801a2e0b918b1cc5c10840fe3b7f
Anderson Bolivar <bolivar.anderson@hotmail.com>
2020-09-06 23:11:16 -0300

    coverage - test: controller scope

---
+++ app/post/blogPostController.test.js
@@ -0,0 +1,10 @@
+const blogPostController = require('./blogPostController');
+
+describe('Blog Post Controller', () => {
+  describe('Create Blog Post', () => {});
+  describe('Find Blog Post', () => {});
+  describe('List Blog Post', () => {});
+  describe('Remove Blog Post', () => {});
+  describe('Search Blog Post', () => {});
+  describe('Update Blog Post', () => {});
+});
---
+++ app/user/userController.test.js
@@ -0,0 +1,10 @@
+const userController = require('./userController');
+
+describe('User Controller', () => {
+  describe('Create User', () => {});
+  describe('Find User', () => {});
+  describe('List User', () => {});
+  describe('Remove User', () => {});
+  describe('Search User', () => {});
+  describe('Update User', () => {});
+});
