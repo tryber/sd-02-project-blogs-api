8ef847979b53d132807ec0e9305757c2c9c94302
richardwellerson <richardwellerson@gmail.com>
2020-10-02 18:07:41 -0300

    Code Climate add Posts

--- seeders/20200812205113-AddUsers.js
+++ seeders/20200812205113-AddUsers.js
@@ -1,15 +1,13 @@
 module.exports = {
   up: async (queryInterface) => queryInterface
-    .bulkInsert('Users',
-      [
-        {
-          displayName: 'Marcos Mion',
-          email: 'marcos@mion.com',
-          password: '123456',
-          image: 'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
-        },
-      ],
-      {}),
+    .bulkInsert('Users', [
+      {
+        displayName: 'Marcos Mion',
+        email: 'marcos@mion.com',
+        password: '123456',
+        image: 'http://4.bp.blogspot.com/_YA50adQ-7vQ/S1gfR_6ufpI/AAAAAAAAAAk/1ErJGgRWZDg/S45/brett.png',
+      }],
+    {}),
 
   down: async (queryInterface) => queryInterface.bulkDelete('Users', null, {}),
 };
